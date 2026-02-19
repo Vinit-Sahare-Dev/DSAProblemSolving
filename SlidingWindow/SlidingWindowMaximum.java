dcd4882a8eac9c811b26c156c129fe2439407d7a
Vinit-Sahare-Dev <vinit.sahare.dev@gmail.com>
2026-02-19 10:39:25 +0530

    folders created

--- SlidingWindow/SlidingWindowMaximum.java
+++ SlidingWindow/SlidingWindowMaximum.java
@@ -1,8 +1,17 @@
-
+/*
+ * You are given an array of integers, arr,
+and an integer k. Your task is to find and print the maximum number in
+each contiguous window of size k.
+Sample test case 1:
+Input:
+arr = [1, 3, -1, -3, 5, 3, 6, 7] k=3
+Output:
+[3, 3, 5, 5, 6, 7]
+ */
 public class SlidingWindowMaximum {
         public static void main (String[] args){
-        int[] arr = { 2,9,3,4,7,6,5,2,3,4};
-        int k = 3;
+        int[] arr = {1, 3, -1, -3, 5, 3, 6, 7} ;      
+         int k = 3;
         int[] arr1 = new int[arr.length - k + 1];
         for(int i = 0; i<arr.length - k+1;i++){
             int max = arr[i];
